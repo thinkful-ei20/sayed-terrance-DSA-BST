1276a9bdbe8be86b8ccc0cb737aeff451705cfaf
Terrance Corley <terrancecorley@outlook.com>
2018-06-26 09:20:30 -0700

    adding binary tree class

---
+++ bst.js
@@ -0,0 +1,79 @@
+// Create a BST class
+// Walk through the Binary Search Tree code in the curriculum and understand it well. Then write a Binary Search Tree class and with its core functions (insert, remove, find) from scratch.
+
+// Create a Binary Search Tree called BST and insert 3,1,4,6,9,2,5,7 to your tree. Compare your result with the result from the first exercise
+
+class BinarySearchTree {
+  constructor(key=null, value=null, parent=null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(key, value) {
+    if (this.key === null) {
+      this.key = key;
+      this.value = value;
+    } else if (key < this.key) {
+      if (this.left === null) {
+        this.left = new BinarySearchTree(key, value, this);
+      } else {
+        this.left.insert(key, value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BinarySearchTree(key, value, this);
+      } else {
+        this.right.insert(key, value);
+      }
+    }
+  }
+
+  find(key) {
+    if (this.key === key) {
+      return this.value;
+    } else if (key < this.key && this.left) {
+      return this.left.find(key);
+    } else if (key > this.key && this.right) {
+      return this.right.find(key);
+    } else {
+      throw new Error('Key Error');
+    }
+  }
+
+  remove(key) {
+    if (this.key === key) {
+      if (this.left && this.right) {
+        const successor = this.right._findMin();
+        this.key = successor.key;
+        this.value = successor.value;
+        successor.remove(successor.key);
+      } else if (this.left) {
+        this._replaceWith(this.right);
+      } else {
+        this._replaceWith(null);
+      }
+    } else if (key < this.key && this.left) {
+      this.left.remove(key);
+    } else if (key > this.key && this.right) {
+      this.right.remove(key);
+    } else {
+      throw new Error('Key Error');
+    }
+  }
+
+}
+
+const BST = new BinarySearchTree();
+BST.insert(3);
+BST.insert(1);
+BST.insert(4);
+BST.insert(6);
+BST.insert(9);
+BST.insert(2);
+BST.insert(5);
+BST.insert(7);
+
+console.log(BST);
